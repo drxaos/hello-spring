f304c8ecb480fc611fa424de063d81b873777ee1
v.polyakov <vladimir.p.polyakov@gmail.com>
2019-10-03 00:42:11 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello(@RequestParam(value = "name", required = false) String name) {
+        if (name == null) {
+            name = "Unknown";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello, ");
+        sb.append(name);
+        sb.append("!");
+        return sb.toString();
+    }
+
+}
